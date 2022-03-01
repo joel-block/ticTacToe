14397781acd6aa42946bd42d3862c430dda216be
Joel <joel.t.block@gmail.com>
2022-03-01 09:53:37 -0600

    initial setup

---
+++ app.js
@@ -0,0 +1,12 @@
+// Game state
+const gameState = {
+    players: ['x', 'o'],
+    board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ],
+    gameStatus: ''
+}
+
+// 
