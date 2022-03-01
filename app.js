ab8e1ebbcd28cef97fd8d360c1cfc0cb36855fcd
Joel <joel.t.block@gmail.com>
2022-03-01 13:56:49 -0600

    added js content, styled css a bit

--- app.js
+++ app.js
@@ -11,9 +11,40 @@ const gameState = {
     gameHistory: {
         wins: 0,
         losses: 0
+    },
+    move: function(piece, row, col) {
+        if (this.board[row][col] === null){
+            this.board[row][col] = piece;
+        }
+        return this.board;
+    },
+    clear: function() {
+        this.board = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
+        return this.board;
     }
 }
 
-let playerName = prompt('What is your name?', '')
-playerName ?? "Challenger"
 // 
+
+// let playerName = prompt('What is your name?', '');
+// playerName ?? "Challenger";
+
+let board = document.querySelector('#board');
+
+let h1 = document.createElement('h1');
+h1.classList.add('header');
+board.appendChild(h1);
+h1.innerText = gameState.gameStatus;
+
+let button = document.createElement('button');
+button.classList.add('start');
+board.appendChild(button);
+button.innerText = 'Begin';
+
+
+// let grid = document.createElement('table');
+// board.appendChild(grid);
