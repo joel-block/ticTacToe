1b52e8e1a9ff4fb66bdf77fbb778f0108a1e504e
Joel <joel.t.block@gmail.com>
2022-03-05 20:42:47 -0600

    created separate game modes, fixed alternating pieces, need to create win detection functions and possibly improve compMove

--- app.js
+++ app.js
@@ -1,85 +1,15 @@
 "use strict";
 
-let playerName = ""; /* prompt('What is your name?', ''); */
-if (playerName === "") {
-  playerName = "Challenger";
-}
-
-// Game state
-const gameState = {
-  player1: "X",
-  player2: "O",
-  board: [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ],
-  gameStatus: `Let's play some Tic-Tac-Toe, ${playerName}!`,
-  gameHistory: {
-    wins: 0,
-    losses: 0,
-  },
-  move: function (piece, row, col) {
-    if (gameState.board[row][col] === null) {
-      gameState.board[row][col] = piece;
-    }
-  },
-  clear: function () {
-    gameState.board = [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null],
-    ];
-  },
-};
-// Helper functions
-function checkCol(event) {
-  let c;
-  if (event.target.matches(".col0")) {
-    c = 0;
-  }
-  if (event.target.matches(".col1")) {
-    c = 1;
-  }
-  if (event.target.matches(".col2")) {
-    c = 2;
-  }
-  return c;
-}
-
-function checkRow(event) {
-  let r;
-  if (event.target.matches(".row0")) {
-    r = 0;
-  }
-  if (event.target.matches(".row1")) {
-    r = 1;
-  }
-  if (event.target.matches(".row2")) {
-    r = 2;
-  }
-  return r;
-}
-
-let currentPlayer = "";
-function randomPlayer() {
-  let x = Math.floor(Math.random() * 2) == 0;
-  if (x) {
-    return gameState.player1;
-  } else {
-    return gameState.player2;
-  }
-}
-
-currentPlayer = randomPlayer();
+// Page Setup
 let board = document.querySelector(".top");
-let grid = document.querySelector(".grid");
+let grid1 = document.querySelector(".grid.grid1"); /* One Player Board */
+let grid2 = document.querySelector(".grid.grid2"); /* Two Player Board */
+let grid3 = document.querySelector(".grid.grid3"); /* Dummy Board */
 
 // Heading
 let h1 = document.createElement("h1");
 h1.classList.add("header");
 board.appendChild(h1);
-h1.innerText = gameState.gameStatus;
 
 let buttonContainer = document.createElement("div");
 buttonContainer.classList.add("button-container");
@@ -95,91 +25,267 @@ buttonTwoPlayer.classList.add("two-player");
 buttonContainer.appendChild(buttonTwoPlayer);
 buttonTwoPlayer.innerText = "Two Players";
 
-let button1 = document.createElement("button");
-button1.classList.add("start");
-buttonContainer.appendChild(button1);
-button1.innerText = "Go again?";
+let buttonAgain = document.createElement("button");
+buttonAgain.classList.add("start");
+buttonContainer.appendChild(buttonAgain);
+buttonAgain.innerText = "Go again?";
 
-let button2 = document.createElement("button");
-button2.classList.add("clear");
-buttonContainer.appendChild(button2);
-button2.innerText = "Clear Board";
+let buttonClear = document.createElement("button");
+buttonClear.classList.add("clear");
+buttonContainer.appendChild(buttonClear);
+buttonClear.innerText = "Clear Board";
 
 let h2 = document.createElement("h2");
 board.appendChild(h2);
 h2.innerText = ". . . . . .";
 
-// Grid
-
-for (let i = 0; i < gameState.board.length; i++) {
-  for (let k = 0; k < gameState.board[i].length; k++) {
+// Grid for One-Player Mode
+for (let i = 0; i < 3; i++) {
+  for (let k = 0; k < 3; k++) {
     let space = document.createElement("span");
-    space.classList.add("space", "row" + i, "col" + k);
-    grid.appendChild(space);
-    space.innerText = gameState.board[i][k];
+    space.classList.add("space", "space1", "row" + i, "col" + k);
+    grid1.appendChild(space);
   }
 }
 
-let space = document.querySelector(".space");
-let spaces = document.querySelectorAll(".space");
+let space1 = document.querySelector(".space1");
+let spaces1 = document.querySelectorAll(".space1");
+
+// Grid for Two-Player Mode
+for (let i = 0; i < 3; i++) {
+  for (let k = 0; k < 3; k++) {
+    let space = document.createElement("span");
+    space.classList.add("space", "space2", "row" + i, "col" + k);
+    grid2.appendChild(space);
+  }
+}
+
+let space2 = document.querySelector(".space2");
+let spaces2 = document.querySelectorAll(".space2");
+
+// Empty Grid to display outside of gameplay
+for (let i = 0; i < 3; i++) {
+  for (let k = 0; k < 3; k++) {
+    let space = document.createElement("span");
+    space.classList.add("space");
+    grid3.appendChild(space);
+  }
+}
+
+// Define Player One's Name
+let playerName = prompt('What is your name?', 'Challenger');
+if (playerName === "") {
+  playerName = "Challenger";
+}
+
+// Define Player Two's Name before prompt
+let player2Name = ""
+
+// Game state
+const gameState = {
+  pieces: ["X", "O"],
+  board: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+  gameStatus: `Let's play some Tic-Tac-Toe, ${playerName}!`,
+  gameHistory: {
+    wins: 0,
+    losses: 0,
+  },
+  renderBoard: function (spaceClass) {
+    for (let i = 0; i < gameState.board.length; i++){
+      for (let k = 0; k < gameState.board[i].length; k++){
+        let space = document.querySelector('.' + spaceClass + '.row' + i + '.col' + k);
+        space.innerText = gameState.board[i][k];
+      }
+    }
+  },
+  move: function (piece, row, col) {
+    if (gameState.board[row][col] === null) {
+      gameState.board[row][col] = piece;
+    }
+  },
+  compMove: function (piece) {
+    let row = Math.floor(Math.random() * 3);
+    let col = Math.floor(Math.random() * 3);
+    if (gameState.board[0].indexOf(null) === -1 &&
+        gameState.board[1].indexOf(null) === -1 &&
+        gameState.board[2].indexOf(null) === -1 ) {
+          return;
+        }
+    if (gameState.board[row][col] === null) {
+      gameState.board[row][col] = piece;
+    } else {
+      gameState.compMove(piece)
+    }
+  },
+  clear: function () {
+    gameState.board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+    for (let i = 0; i < spaces1.length; i++) {
+      spaces1[i].innerText = null;
+    }
+    for (let i = 0; i < spaces2.length; i++) {
+      spaces2[i].innerText = null;
+    }
+  },
+  /* Board positions with index of spaces[i]:
+  [0], [1], [2]
+  [3], [4], [5]
+  [6], [7], [8] */
+  winCombos: [
+    // rows
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // columns
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // diagonals
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+};
+
+
+
+// Helper functions
+function checkCol(event) {
+  let col;
+  if (event.target.matches(".col0")) {
+    col = 0;
+  }
+  if (event.target.matches(".col1")) {
+    col = 1;
+  }
+  if (event.target.matches(".col2")) {
+    col = 2;
+  }
+  return col;
+}
+
+function checkRow(event) {
+  let row;
+  if (event.target.matches(".row0")) {
+    row = 0;
+  }
+  if (event.target.matches(".row1")) {
+    row = 1;
+  }
+  if (event.target.matches(".row2")) {
+    row = 2;
+  }
+  return row;
+}
+
+function checkWinOne() {
+
+}
+
+function checkWinTwo() {
+
+}
+
+function randomPlayer() {
+  let x = Math.floor(Math.random() * 2) == 0;
+  if (x) {
+    return gameState.pieces[0];
+  } else {
+    return gameState.pieces[1];
+  }
+}
+
+function switchPlayer() {
+  if (currentPlayer === gameState.pieces[0]){
+    currentPlayer = gameState.pieces[1];
+  } else {
+    currentPlayer = gameState.pieces[0];
+  }
+}
+
+let currentPlayer = randomPlayer();
+h1.innerText = gameState.gameStatus;
 
 // Event Listeners
 
 buttonOnePlayer.addEventListener("click", function () {
+  grid3.style.display = "none";
   gameState.gameStatus = "Challenge accepted!";
   h1.innerText = gameState.gameStatus;
-  h2.innerText = `${playerName} controls the ${currentPlayer}'s`;
+  h2.innerText = `${playerName} controls the ${currentPlayer}'s.`;
   this.style.display = "none";
   buttonTwoPlayer.style.display = "none";
-  button2.style.display = "flex";
-  grid.addEventListener("click", function (event) {
+  buttonClear.style.display = "flex";
+  grid1.style.display = "flex";
+  // One Player Game Mode
+  grid1.addEventListener("click", function (event) {
     let row = checkRow(event);
     let col = checkCol(event);
     gameState.move(currentPlayer, row, col);
-    event.target.innerText = gameState.board[row][col];
+    gameState.renderBoard('space1');
+    switchPlayer();
+    h2.innerText = `Computer controls the ${currentPlayer}'s.`
+    // Computer's turn
+    setTimeout( () => {
+      gameState.compMove(currentPlayer);
+      gameState.renderBoard('space1');
+      switchPlayer();
+      h2.innerText = `${playerName} controls the ${currentPlayer}'s.`;
+    }, 1500);
   });
-  grid.style.display = "flex";
 });
 
 buttonTwoPlayer.addEventListener("click", function () {
+  grid3.style.display = "none";
+  player2Name = prompt(`What is Player 2's name?`, 'Opponent');
+  if (player2Name === "") {
+    player2Name = "Opponent";
+  }
+
   gameState.gameStatus = "Let the game begin!";
   h1.innerText = gameState.gameStatus;
-  h2.innerText = `${currentPlayer}'s move first`;
+  h2.innerText = `It is ${playerName}'s turn!`;
   this.style.display = "none";
   buttonOnePlayer.style.display = "none";
-  button2.style.display = "flex";
-  grid.addEventListener("click", function (event) {
+  buttonClear.style.display = "flex";
+  grid2.style.display = "flex";
+
+  // Two Player Game Mode
+  grid2.addEventListener("click", function (event) {
     let row = checkRow(event);
     let col = checkCol(event);
     gameState.move(currentPlayer, row, col);
-    event.target.innerText = gameState.board[row][col];
+    gameState.renderBoard('space2');
+    switchPlayer();
+    if (h2.innerText === `It is ${playerName}'s turn!`) {
+      h2.innerText = `It is ${player2Name}'s turn!`;
+    } else {
+      h2.innerText = `It is ${playerName}'s turn!`;
+    }
   });
-  grid.style.display = "flex";
 });
 
-button2.addEventListener("click", function () {
+buttonClear.addEventListener("click", function () {
   gameState.gameStatus = `Wanna play another round, ${playerName}?`;
   h1.innerText = gameState.gameStatus;
   h2.innerText = "Let's play some more!";
   gameState.clear();
   this.style.display = "none";
-  button1.style.display = "flex";
+  buttonAgain.style.display = "flex";
   currentPlayer = randomPlayer();
-  for (let i = 0; i < spaces.length; i++) {
-    spaces[i].innerText = null;
-  }
-  grid.style.display = "none";
+  grid1.style.display = "none";
+  grid2.style.display = "none";
+  grid3.style.display = "flex";
 });
 
-button1.addEventListener("click", function () {
+buttonAgain.addEventListener("click", function () {
   buttonOnePlayer.style.display = "flex";
   buttonTwoPlayer.style.display = "flex";
   this.style.display = "none";
 });
-
-// grid.addEventListener('click', function(event){
-//     let row = checkRow(event);
-//     let col = checkCol(event);
-//     gameState.move(currentPlayer, row, col);
-//     event.target.innerText = gameState.board[row][col];
-// })
